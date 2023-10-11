c700df493cad86017a44f188fcb384cba6d72db1
zotowa <122356425+zotowa@users.noreply.github.com>
2023-10-11 08:48:12 +0300

    Delete appointment directory

--- appointment/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import AppointmentView
-
-app_name = 'appointments'
-
-urlpatterns = [
-    path('make_appointment/', AppointmentView.as_view(), name="make_appointment"),
-]
